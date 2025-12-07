4ead1f58c8fdb37e37536b6e4483929e24780f36
SRK SHADHIN <mt6w6v6x5b@privaterelay.appleid.com>
2025-12-07 15:48:19 +0600

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-// Minimal express server to serve static files (for local testing)
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname)));
-app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'index.html')));
-app.listen(port, ()=> console.log('Server running on http://localhost:'+port));
